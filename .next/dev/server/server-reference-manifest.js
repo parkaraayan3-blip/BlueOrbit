4d4a0567396bfbdec07170d9ab0cf794daf40547
Aayan_parkar <parkaraayan3@gmail.com>
2026-06-12 04:06:00 +0530

    chore: untrack Next.js build cache and update gitignore

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"pRfzshTsDj+pE+WvX9dqm9z1zNziUO4swj9VwPNYxEw=\"\n}"
--- .next/dev/static/chunks/src_app_page_jsx_0-f467x._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_20mgdhy._.js","static/chunks/node_modules_1e9pq1q._.js"],
-    source: "dynamic"
-});
